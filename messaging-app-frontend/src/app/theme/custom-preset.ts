4397960d521974ad4d5b45d95064b73d7eacd0cf
AFaseeh <AbdelrahmanFaseeh@gmail.com>
2026-06-28 18:14:38 +0300

    feat(ui): styling chat page and global preset colors

--- messaging-app-frontend/src/app/theme/custom-preset.ts
+++ messaging-app-frontend/src/app/theme/custom-preset.ts
@@ -1,7 +1,111 @@
 import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura';
 
-const customPreset = definePreset(Aura, {   
+const customPreset = definePreset(Aura, {
+  primitive: {
+    violet: {
+      50: '#f5f3ff',
+      100: '#ede9fe',
+      200: '#ddd6fe',
+      300: '#c4b5fd',
+      400: '#a78bfa',
+      500: '#8b5cf6',
+      600: '#7c3aed',
+      700: '#6d28d9',
+      800: '#5b21b6',
+      900: '#4c1d95',
+      950: '#2e1065'
+    },
+    slate: {
+      0: '#ffffff',
+      50: '#f8fafc',
+      100: '#f1f5f9',
+      200: '#e2e8f0',
+      300: '#cbd5e1',
+      400: '#94a3b8',
+      500: '#64748b',
+      600: '#475569',
+      700: '#334155',
+      800: '#1e293b',
+      900: '#0f172a',
+      950: '#020617'
+    }
+  },
+  
+  semantic: {
+    primary: {
+      50: '{violet.50}',
+      100: '{violet.100}',
+      200: '{violet.200}',
+      300: '{violet.300}',
+      400: '{violet.400}',
+      500: '{violet.500}',
+      600: '{violet.600}',
+      700: '{violet.700}',
+      800: '{violet.800}',
+      900: '{violet.900}',
+      950: '{violet.950}'
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: '{slate.0}',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}',
+
+          ground: '{slate.50}',
+          card: '{slate.0}'
+        }
+      },
+      dark: {
+        surface: {
+          0: '{slate.950}',
+          50: '{slate.900}',
+          100: '{slate.800}',
+          200: '{slate.700}',
+          300: '{slate.600}',
+          400: '{slate.500}',
+          500: '{slate.400}',
+          600: '{slate.300}',
+          700: '{slate.200}',
+          800: '{slate.100}',
+          900: '{slate.50}',
+          950: '{slate.0}',
+
+          ground: '{slate.950}',
+          card: '{slate.900}',
+          input: '{slate.800}',
+        }
+      }
+    }
+  },
+  
+  components: {
+    inputtext:{
+      root: {
+        background: '{surface.input}',
+        color: '{surface.900}'
+      }
+    },
+    splitter: {
+      root: {
+        background: '{surface.ground}',
+        color: '{surface.900}'
+      },
+      gutter: {
+        background: '{surface.200}'
+      }
+    }
+  }
 });
 
 export default customPreset;
